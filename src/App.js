7d2bfeac606a91541f21160be87b3b7c077cacb0
Tincho <mardsn@gmail.com>
2021-12-03 20:41:08 -0300

    Cambio en menú de navegacion

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ function App() {
       <ul>
         <li className="menu">Home</li>
         <li className="menu">About</li>
-        <li className="menu">Photos</li>
-        <li className="menu">Videos</li>
-        <li className="menu">Contacto</li>
       </ul>
     </div>
   );
