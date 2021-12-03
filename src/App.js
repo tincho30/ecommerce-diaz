76be3a2a184062580b903387827f2a3c6c1e96f6
Tincho <mardsn@gmail.com>
2021-12-03 19:06:57 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <li className="menu">Home</li>
+        <li className="menu">About</li>
+        <li className="menu">Photos</li>
+        <li className="menu">Videos</li>
+        <li className="menu">Contacto</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
