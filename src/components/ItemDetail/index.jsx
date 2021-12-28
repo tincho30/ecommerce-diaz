630d0dedfc4c30bae9b455de54b3042d433b672e
Tincho <mardsn@gmail.com>
2021-12-28 15:41:32 -0300

    Category, ItemDetail, ItemDetailContainer

--- src/components/ItemDetail/index.jsx
+++ src/components/ItemDetail/index.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import {styles} from './styles'
 
 const index = ({prodId,title,year,pictureUrl,price,category}) => {
     return (
-        <div>
-            <img src={pictureUrl} alt={title}/>
-            <section>
-                <h2>{title}</h2>
-                <h3>{price}</h3>
-                <h4>{category}</h4>
-            </section>
-        </div>
+        <>
+        <table style={styles.detailCard}>
+        <th rowspan="4"><img style={styles.detailImg} src={pictureUrl} alt={title}/>
+        </th>
+        <th>
+                <tr>
+                    <span style={styles.detailTitle}>{title}</span><br></br>
+                    <span style={styles.detailPrice}>{"$"+price}</span><br></br>
+                    {category}
+                </tr>
+        </th>
+        </table>
+        </>
     )
 }
 
